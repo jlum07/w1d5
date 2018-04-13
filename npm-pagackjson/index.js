6f1eae3c4d08cf8e21a1129ab03010a4ac628489
Justin Lum <j.lum07@gmail.com>
2018-04-13 14:51:53 -0400

    ?

--- npm-pagackjson/index.js
+++ npm-pagackjson/index.js
@@ -2,3 +2,6 @@ var chalk = require("chalk");
 
 var message = "Hello " + chalk.yellow("World");
 console.log(message);
+
+var fox = chalk.bgYellow("The ") + chalk.yellow("quick ") + chalk.green("brown ") + chalk.red("fox ") + chalk.blue("jumped");
+console.log(fox);
